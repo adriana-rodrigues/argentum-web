d3669304ba9b70e53e920801799f1e86a3da2a14
Adriana Rodrigues <58852743+adriana-rodrigues@users.noreply.github.com>
2020-06-05 17:27:37 -0300

    Update and rename src/test/java/br/com/caelum/argentum/indicadores/MediaMovelSimplesTest.java to src/teste/Java/br/com/caelum/argentum/indicadores/MediaMovelSimplesTest.java

--- src/teste/Java/br/com/caelum/argentum/indicadores/MediaMovelSimplesTest.java
+++ src/teste/Java/br/com/caelum/argentum/indicadores/MediaMovelSimplesTest.java
@@ -17,7 +17,7 @@ public class MediaMovelSimplesTest {
 		assertEquals(2.0, mms.calcula(2, serie),      0.000001);
 		assertEquals(3.0, mms.calcula(3, serie),      0.000001);
 		assertEquals(10.0 / 3, mms.calcula(4, serie), 0.000001);
-		assertEquals(11.0 / 3, mms.calcula(5, serie), 0.000001);
+		assertEquals(1 / 3, mms.calcula(5, serie), 0.000001);
 		assertEquals(4.0, mms.calcula(6, serie),      0.000001);
 		assertEquals(13.0 / 3, mms.calcula(7, serie), 0.000001);
 		assertEquals(4.0, mms.calcula(8, serie),      0.000001);
